7a7b8098685dd27acea89b2edfa0a9fa98bdb7ea
Julio Sarango <jsarangoq@gmail.com>
2017-06-06 17:28:27 -0500

    first commit

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [
+
+  ]
+})
+export class TemplateComponent implements OnInit {
+
+  usuario:Object = {
+    nombre: null,
+    apellido: null,
+    email: null,
+    pais:""
+  }
+
+  paises:Object = [
+    { codigo: "Ec",
+      nombre: "Ecuador"
+    },
+    { codigo: "COL",
+      nombre: "Colombia"
+    },
+    { codigo: "AR",
+      nombre: "Argentina"
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma:NgForm) {
+    console.log('formulario posteado');
+    console.log("NgForm: ", forma);
+    console.log("Value: ",forma.value);
+  }
+
+}
